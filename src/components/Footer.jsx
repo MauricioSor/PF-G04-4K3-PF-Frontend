a2a84f4a9c323a2c94e146dfe75e18ff284ea37f
Mauricio Soria <mauricioutn2017@gmail.com>
2026-05-23 23:52:30 -0300

    Feature: system model

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div>
+            <section className='bg-dark text-center text-light py-5'>
+            <span> &copy; Todos los derechos Reservados- MauricioSor</span>
+            </section>
+        </div>
+    );
+};
+
+export default Footer;
