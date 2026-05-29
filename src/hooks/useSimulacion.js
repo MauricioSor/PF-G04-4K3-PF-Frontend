24214320de6371581122507cad29d77a7df01e58
Pablo962 <Pablo.Mamani@alu.frt.utn.edu.ar>
2026-05-28 21:44:19 -0300

    configs

--- src/hooks/useSimulacion.js
+++ src/hooks/useSimulacion.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const API_BASE = 'http://localhost:3001/api'
+const API_BASE = import.meta.env.VITE_API_BASE || 'http://localhost:3001/api'
 
 export function useSimulacion() {
   const [view,       setView]       = useState('bienvenida')
